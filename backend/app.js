c97203dcde13c9279d30c2e7c5502081023aab67
Tina Choudary Yalamanchili <tinachoudary1762@gmail.com>
2026-06-07 22:57:48 +0530

    changes backend

--- backend/app.js
+++ backend/app.js
@@ -76,6 +76,8 @@ ${question}
   }
 });
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
